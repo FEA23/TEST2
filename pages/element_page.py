3c9084543d264144fd8a1c06adcc16b456e6f2ca
Edgar Fanyan <meroin@mail.ru>
2023-07-13 16:39:18 +0300

    update elements test added test radio butten

--- pages/element_page.py
+++ pages/element_page.py
@@ -1,7 +1,8 @@
+import random
 import time
-
+from selenium.webdriver.common.by import By
 from generator.generator import generated_person
-from locators.locators_pages import TextBoxPageLocators
+from locators.locators_pages import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_pages import BasePage
 
 class TextBoxPage(BasePage):
@@ -26,3 +27,43 @@ class TextBoxPage(BasePage):
         current_address = self.element_is_present(self.locators.CREATED_Current_Address).text.split(':')[1]
         permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
+
+class CheckBoxPage(BasePage):
+
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        count = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                count -= 1
+            else:
+                break
+
+    # def get_checked_checkbox(self):
+    #     cheked_list = self.element_are_present(self.locators.CHECKED_ITEMS)
+    #     data = []
+    #     for box in cheked_list:
+    #         title_item = box.find_element_by_xpath(self.locators.TITLE_ITEM)
+    #         print(title_item.text)
+    #         #data.append(box.text)
+    #     #print(data)
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_on_the_radio_button(self, choice):
+        choices = {'yes':  self.locators.YES,
+                'impressive' :self.locators.IMPRESSIVE,
+                'no': self.locators.NO}
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
--- tests/test_one.py
+++ tests/test_one.py
@@ -1,5 +1,7 @@
 import time
-from pages.element_page import TextBoxPage
+from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+
+
 
 
 
@@ -12,3 +14,26 @@ class TestElements:
             input_data = text_box.fill_all_fields()
             output_data = text_box.check_filled_form()
             assert input_data == output_data
+
+    class TestCheckBox:
+        def test_check_box(self,driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            #check_box_page.get_checked_checkbox()
+
+    class TestRadioButton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes'
+            assert output_impressive == 'Impressive'
+            assert output_no == 'No', '"No" have not been selected'
